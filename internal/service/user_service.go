dcba3b8d747a2d523bcf5f111f2b884e97ae26fc
Sudhesh Hegde <hegdesud2002@gmail.com>
2025-12-14 10:47:47 +0530

    user service method to verify date format and create user

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -10,3 +10,20 @@ type UserService struct {
 	repo *repository.UserRepository
 }
 
+func NewUserService(r *repository.UserRepository) *UserService {
+	return &UserService{repo: r}
+}
+
+func (s *UserService) CreateUser(
+	ctx context.Context,
+	name string,
+	dobStr string,
+)error{
+	_, err := time.Parse("2006-01-02", dobStr)
+	if err != nil {
+		return err
+	}
+
+	_, err = s.repo.CreateUser(ctx, name, dobStr)
+	return err
+}
